08a3cb8518ca75a95183769050f1337984f2ecaf
Illia Kyselov <illya.kis99@gmail.com>
2023-07-05 21:50:31 +0300

    added new features

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,72 @@ fetch("/places")
       maxZoom: 18,
     }).addTo(map);
 
+    var marker;
+
+    function addMarkerToMap(event) {
+      const { lat, lng } = event.latlng;
+
+      if (marker) {
+        map.removeLayer(marker);
+      }
+
+      marker = L.marker([lat, lng]).addTo(map);
+
+      marker.on("click", function () {
+        const form = document.createElement("form");
+        form.innerHTML = `
+          <input type="text" class="form-field" name="name" placeholder="Назва" required />
+          <input type="text" class="form-field" name="description" placeholder="Опис" required />
+          <input type="number" name="latitude" value="${lat.toFixed(
+            6
+          )}" step="any" hidden />
+          <input type="number" name="longitude" value="${lng.toFixed(
+            6
+          )}" step="any" hidden />
+          <input type="date" name="createdate" value="${new Date().toLocaleDateString()}" hidden/>
+          <button type="submit" class="button">Додати</button>
+        `;
+
+        form.addEventListener("submit", function (event) {
+          event.preventDefault();
+
+          const formData = new FormData(form);
+          const place = Object.fromEntries(formData.entries());
+
+          addPlaceToTable(place);
+
+          fetch("/places", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(place),
+          })
+            .then(function (response) {
+              if (response.ok) {
+                return response.json();
+              }
+              throw new Error("Error adding place");
+            })
+            .then(function (data) {
+              console.log(data);
+              location.reload();
+            })
+            .catch(function (error) {
+              console.error("An error occurred:", error);
+            });
+
+          map.closePopup();
+          map.removeLayer(marker);
+          marker = null;
+        });
+
+        marker.bindPopup(form).openPopup();
+      });
+    }
+
+    map.on("click", addMarkerToMap);
+
     places.forEach(function (place) {
       var marker = L.marker([place.latitude, place.longitude]).addTo(map);
       marker.bindPopup("<b>" + place.name + "</b><br>" + place.description);
@@ -19,3 +85,188 @@ fetch("/places")
   .catch(function (error) {
     console.error("An error occurred:", error);
   });
+
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+
+function addPlaceToTable(place) {
+  const row = document.createElement("tr");
+  row.setAttribute("data-id", place.id);
+  row.innerHTML = `
+  <td>${place.id}</td>
+  <td>${place.name}</td>
+  <td>${place.description}</td>
+  <td>${place.latitude}</td>
+  <td>${place.longitude}</td>
+  <td>${new Date(place.createdate).toLocaleDateString("uk-UA")}</td>
+  <td><button class="delete-icon" data-id="${place.id}">Delete</button></td>
+`;
+
+  placesTableBody.appendChild(row);
+}
+
+function clearPlacesTable() {
+  placesTableBody.innerHTML = "";
+}
+
+function fetchPlaces(sortColumn = "id", sortOrder = "asc") {
+  const searchQuery = searchInput.value.trim();
+
+  const sortParam = `sort=${sortColumn}&order=${sortOrder}`;
+
+  let fetchUrl = `/places?${sortParam}`;
+  if (searchQuery) {
+    fetchUrl += `&search=${encodeURIComponent(searchQuery)}`;
+  }
+
+  const deletedPlaceId = localStorage.getItem("deletedPlaceId");
+  if (deletedPlaceId) {
+    fetch(`/places/${deletedPlaceId}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .catch(function (error) {
+        console.error("An error occurred:", error);
+      });
+
+    localStorage.removeItem("deletedPlaceId");
+  }
+
+  fetch(fetchUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (places) {
+      clearPlacesTable();
+      places.forEach(function (place) {
+        addPlaceToTable(place);
+      });
+    })
+    .catch(function (error) {
+      console.error("An error occurred:", error);
+    });
+}
+
+searchButton.addEventListener("click", function () {
+  const searchQuery = searchInput.value.trim();
+
+  fetch(`/places?search=${encodeURIComponent(searchQuery)}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (places) {
+      clearPlacesTable();
+      places.forEach(function (place) {
+        addPlaceToTable(place);
+      });
+    })
+    .catch(function (error) {
+      console.error("An error occurred:", error);
+    });
+});
+
+fetchPlaces();
+
+function deletePlaceFromTable(event) {
+  const deleteButton = event.target;
+  const row = deleteButton.closest("tr");
+  if (row) {
+    const placeId = row.getAttribute("data-id");
+
+    fetch(`/places/${placeId}`, {
+      method: "DELETE",
+    })
+      .then(function (response) {
+        if (response.ok) {
+          row.remove();
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then(function () {
+        fetchPlaces();
+        location.reload();
+      })
+      .catch(function (error) {
+        console.error("An error occurred:", error);
+      });
+  }
+}
+
+document
+  .querySelector("#placesTable tbody")
+  .addEventListener("click", function (event) {
+    if (event.target && event.target.classList.contains("delete-icon")) {
+      deletePlaceFromTable(event);
+    }
+  });
+
+function fetchHistory() {
+  fetch("/history")
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Error fetching history");
+      }
+      return response.json();
+    })
+    .then(function (history) {
+      history.forEach(function (event) {
+        addEventToHistoryTable(event);
+      });
+    })
+    .catch(function (error) {
+      console.error("An error occurred:", error);
+    });
+}
+
+window.addEventListener("load", function () {
+  fetchHistory();
+});
+
+function addEventToHistoryTable(event) {
+  const row = document.createElement("tr");
+  const formattedTimestamp = new Date(event.timestamp).toLocaleString("uk-UA");
+  row.innerHTML = `
+    <td>${event.id}</td>
+    <td>${event.name}</td>
+    <td>${event.action}</td>
+    <td>${formattedTimestamp}</td>
+  `;
+  historyTableBody.appendChild(row);
+}
+
+function addEventToHistory(id, name, action) {
+  const event = {
+    id: id,
+    name: name,
+    action: action,
+    timestamp: new Date().toLocaleDateString("uk-UA", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+      timeZone: "Europe/Kiev",
+    }),
+  };
+
+  addEventToHistoryTable(event);
+
+  fetch("/history", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(event),
+  })
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Error adding event");
+      }
+    })
+    .catch(function (error) {
+      console.error("An error occurred:", error);
+    });
+}
--- server.js
+++ server.js
@@ -18,7 +18,82 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/places", (req, res) => {
-  const query = "SELECT * FROM places";
+  const searchQuery = req.query.search || "";
+
+  let query = "SELECT * FROM places";
+  let values = [];
+
+  if (searchQuery) {
+    query += " WHERE name ILIKE $1 OR description ILIKE $1";
+    values = [`%${searchQuery}%`];
+  }
+
+  client.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred" });
+      return;
+    }
+
+    const rows = result.rows;
+    res.json(rows);
+  });
+});
+
+app.post("/places", (req, res) => {
+  const { name, description, latitude, longitude } = req.body;
+
+  const query =
+    "INSERT INTO places (name, description, latitude, longitude, createdate) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+  const values = [
+    name,
+    description,
+    latitude,
+    longitude,
+    new Date().toISOString().slice(0, 10),
+  ];
+
+  client.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred" });
+      return;
+    }
+
+    const newPlace = result.rows[0];
+
+    const historyQuery =
+      "INSERT INTO history (name, action, timestamp) VALUES ($1, $2, $3)";
+    const historyValues = [
+      newPlace.name,
+      "Created",
+      new Date().toLocaleDateString("uk-UA", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+        timeZone: "Europe/Kiev",
+      }),
+    ];
+
+    client.query(historyQuery, historyValues, (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+
+      res.json(newPlace);
+    });
+  });
+});
+
+app.get("/history", (req, res) => {
+  const sortColumn = req.query.sort || "id";
+  const sortOrder = req.query.order || "asc";
+
+  const query = `SELECT * FROM history ORDER BY ${sortColumn} ${sortOrder}`;
 
   client.query(query, (err, result) => {
     if (err) {
@@ -32,17 +107,12 @@ app.get("/places", (req, res) => {
   });
 });
 
-app.use(express.static(__dirname + "/"));
+app.post("/history", (req, res) => {
+  const { id, name, action, timestamp } = req.body;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.post("/places", (req, res) => {
-  const { name, description, latitude, longitude } = req.body;
   const query =
-    "INSERT INTO places (name, description, latitude, longitude) VALUES ($1, $2, $3, $4)";
-  const values = [name, description, latitude, longitude];
+    "INSERT INTO history (id, name, action, timestamp) VALUES ($1, $2, $3, $4)";
+  const values = [id, name, action, timestamp];
 
   client.query(query, values, (err) => {
     if (err) {
@@ -51,14 +121,53 @@ app.post("/places", (req, res) => {
       return;
     }
 
-    res.json({ message: "Place added successfully" });
+    res.sendStatus(200);
   });
 });
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:* http://127.0.0.1:*"
-  );
-  next();
+app.delete("/places/:id", (req, res) => {
+  const placeId = req.params.id;
+  const query = "DELETE FROM places WHERE id = $1 RETURNING *";
+  const values = [placeId];
+
+  client.query(query, values, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred" });
+      return;
+    }
+
+    const deletedPlace = result.rows[0];
+
+    const historyQuery =
+      "INSERT INTO history (name, action, timestamp) VALUES ($1, $2, $3)";
+    const historyValues = [
+      deletedPlace.name,
+      "Deleted",
+      new Date().toLocaleDateString("uk-UA", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+        timeZone: "Europe/Kiev",
+      }),
+    ];
+
+    client.query(historyQuery, historyValues, (err, result) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+
+    res.sendStatus(204);
+  });
+});
+
+app.use(express.static(__dirname + "/"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
